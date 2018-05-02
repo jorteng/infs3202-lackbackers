03bce3dad22f2a6d53d04fd78b948e5c02419e8d
Isaac <s4452775@student.uq.edu.au>
2018-05-02 15:38:04 +1000

    Added error messages to login page

--- accounts/login.php
+++ accounts/login.php
@@ -6,13 +6,13 @@ include '../includes/header.php';
 
 //Define variables and initialize with empty values
 $email = $password = $user_id = $user_type = "";
-$email_err = $password_err = $userID_err = $userType_err = "";
+$errors = $email_err = $password_err = $userID_err = $userType_err = "";
 
 //Process form data
 if ($_POST) {
     // form validation
     if (empty($_POST['email']) || empty($_POST['password'])) {
-        $errors[] = 'You must provide email and password.';
+        $errors = 'You must provide both email and password.';
     } else {
         // check if email exist in the databse
         $stmt = $link->prepare("SELECT * FROM users WHERE email=?");
@@ -21,20 +21,20 @@ if ($_POST) {
         $stmt->store_result();
 
         if ($stmt->num_rows() < 1) {
-            $errors[] = 'That username doesn\'t exist in our database.';
+            $errors = 'That username doesn\'t exist in our database.';
         } else {
             // Check if password matches
             $stmt->execute();
             $result = $stmt->get_result();
             $user = $result->fetch_assoc();
             if (!password_verify($_POST['password'], $user['password'])) {
-                $errors[] = 'The password does not match our records. Please try again.';
+                $errors = 'The password does not match our records. Please try again.';
             }
         }
     }
     // check for errors
     if (!empty($errors)) {
-        echo '<div class="alert alert-danger">' . $errors[0] . '</div>';
+        echo '<div class="alert alert-danger">' . $errors. '</div>';
     } else {
         // log user in
 		$email = $user['email'];
@@ -50,7 +50,7 @@ if ($_POST) {
             $result = $stmt->get_result();
             $owner = $result->fetch_assoc();
 			$name = $owner['companyName'];
-			$own_id = $owner['owner_id']; 
+			$own_id = $owner['owner_id'];
 		}
 		else if($user_type == 2)
 		{
@@ -80,15 +80,14 @@ if ($_POST) {
         <div class="col-sm-3">
         <h2>Login</h2>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group <?php echo (!empty($errors)) ? 'has-error' : ''; ?>">
                 <label>Email</label>
                 <input type="text" name="email"class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $email_err; ?></span>
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group <?php echo (!empty($errors)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
+                <span class="help-block"><?php echo $errors; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
