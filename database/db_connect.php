cb592e5197fbbe9675b4b5454466413d4e79e539
Isaac <s4452775@student.uq.edu.au>
2018-05-25 09:44:29 +1000

    amended database connectivity file

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,10 +1,11 @@
 <?php
-   require_once 'credentials.php';
-    //Create connection
-    $link = mysqli_connect($servername, $username, $password, $db_name);
 
-    // Check connection
-    if (mysqli_connect_errno()) {
-      echo "Failed to connect to database: " . mysqli_connect_error();
-    }
-    ?>
+require_once 'credentials.php';
+//Create connection
+$link = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($link->connect_error) {
+  die ("Failed to connect to database: " . $link->connect_error);
+}
+?>
